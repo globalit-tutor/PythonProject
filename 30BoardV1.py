a96f703e59248a3d9c64e94b65aaebd8b51e655b
globalit-tutor <zzyzigy250508@gmail.com>
2025-11-17 18:21:18 +0900

    게시판V1 함수 기반 성적 처리 V6 수정 파일입출력 employees.csv summermedals.csv

---
+++ 30BoardV1.py
@@ -0,0 +1,38 @@
+# 게시판 앱 V1
+# "글번호, 글제목, 작성자, 본문, 조회수, 작성일"형식의 
+# 게시판 데이터를 생성,조회,수정,삭제하는 프로그램
+# 새글쓰기 : 글제목, 작성자, 본문
+# 게시판 목록 : 글번호, 글제목, 작성자, 조회수, 작성일(년월일)
+# 본문보기 : 글번호, 글제목, 작성자, 조회수, 작성일, 본문
+
+from zzyzzy.boardv1_lib import menus
+from zzyzzy.boardv1_lib import write_board
+from zzyzzy.boardv1_lib import list_board
+from zzyzzy.boardv1_lib import view_board
+from zzyzzy.boardv1_lib import modify_board
+from zzyzzy.boardv1_lib import remove_board
+
+boards = []
+
+while True:
+    job = input(menus)
+
+    match job:
+        case '1':
+            write_board(boards)
+
+        case '2':
+            list_board(boards)
+
+        case '3':
+            view_board(boards)
+
+        case '4':
+            modify_board(boards)
+
+        case '5':
+            remove_board(boards)
+
+        case '0': break
+        case _: print('번호를 잘못 입력하셨습니다!!')
+
---
+++ zzyzzy/boardv1_lib.py
@@ -0,0 +1,92 @@
+
+counts = 1
+
+menus = f'''
+-----------------------
+     게시판 프로그램 V1
+-----------------------
+  1. 새글 쓰기
+  2. 게시글 목록
+  3. 게시글 본문보기
+  4. 게시글 수정하기
+  5. 게시글 삭제하기
+  0. 프로그램 종료
+-----------------------
+작업을 선택하세요 : '''
+
+header1 = '''
+        ===== 게시글 목록 =====
+번호 |   제목   | 작성자 |  작성일  | 조회
+--------------------------------------         
+'''
+
+
+def input_board():
+    global counts    # 전역변수 함수내 수정 허용
+    title = input('글제목 : ')
+    userid = input('작성자 : ')
+    contents = input('본문 : ')
+
+    board = [counts, title, userid, contents, 0, '2025-11-14 17:47:35']
+    counts += 1
+    return board
+
+
+def write_board(boards):
+    board = input_board()
+    boards.append(board)
+
+    print('✨ 글이 등록되었습니다! ✨')
+
+
+def list_board(boards):
+    result = ''
+    for bd in boards:
+        result += f'{bd[0]} {bd[1]} {bd[2]} {bd[5][:10]} {bd[4]}\n'
+
+    print(f'{header1}{result}')
+
+
+def view_board(boards):
+    bno = int(input('조회할 글번호를 입력하세요: '))
+    result = '🚀 해당 게시물이 존재하지 않습니다!'
+
+    for bd in boards:
+        if bd[0] == bno:
+            result = '\n===== 본문 내용 =====\n'
+            result += f'글번호 : {bd[0]}\n'
+            result += f'제목 : {bd[1]}\n'
+            result += f'작성자 : {bd[2]}\n'
+            result += f'조회수 : {bd[4]}\n'
+            result += f'작성일 : {bd[5]}\n'
+            result += f'본문 : {bd[3]}\n'
+
+    print(result)
+
+
+def modify_board(boards):
+    bno = int(input('수정할 글번호를 입력하세요: '))
+    result = '🚀 해당 게시물이 존재하지 않습니다!'
+
+    for bd in boards:
+        if bd[0] == bno:
+            new_title = input(f'새 제목 ({bd[1]}): ')
+            new_contents = input(f'새 본문 ({bd[3]}): ')
+            bd[1] = new_title
+            bd[3] = new_contents
+            result = '🎉 해당 게시물이 수정되었습니다!'
+
+    print(result)
+
+
+def remove_board(boards):
+    bno = int(input('삭제할 글번호를 입력하세요: '))
+    result = '🚀 해당 게시물이 존재하지 않습니다!'
+
+    for bd in boards:
+        if bd[0] == bno:
+            boards.remove(bd)   # !!
+            result = '🎉 해당 게시물을 삭제했습니다!'
+
+    print(result)
+
